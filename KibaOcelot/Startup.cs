865a9a00f055a2182daf97df0f3eddaf909bad56
liujin <liujin_hzzh@126.com>
2019-12-30 17:01:36 +0800

    KibaOcelot

---
+++ KibaOcelot/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace KibaOcelot
+{
+    /*
+     Nuget搜索Ocelot，安装（如要配合使用Consul，下载Ocelot.Provider.Consul和Ocelot.Provider.Polly，命名空间using Ocelot.Provider.Consul;using Ocelot.Provider.Polly;）
+     然后引用命名空间using Ocelot.DependencyInjection;using Ocelot.Middleware;
+     使用oceloat.josn配置
+
+     */
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+            builder.SetBasePath(environment.ContentRootPath)
+                   .AddJsonFile("appsettings.json", false, reloadOnChange: true)
+                   .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                   .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+            Configuration = builder.Build(); 
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration);//有参数的ocelot是需要引用Ocelot.Provider.Consul和Ocelot.Provider.Polly的，因为有参数的是和Consul查询服务结合的
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 加上async，这样内部可以使用await
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            await app.UseOcelot();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Product/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Product.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Product"; 
+        }
+    }
+}
