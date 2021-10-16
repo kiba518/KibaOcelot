deb8252a2a45c38a4b902630203e518bea9d9748
kiba518 <liujin_hzzh@126.com>
2021-10-16 11:23:49 +0800

    增加配置consul
    增加配置consul

--- KibaOcelot/Startup.cs
+++ KibaOcelot/Startup.cs
@@ -23,26 +23,28 @@ namespace KibaOcelot
      */
     public class Startup
     {
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
-        {
-            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
-            builder.SetBasePath(environment.ContentRootPath)
-                   .AddJsonFile("appsettings.json", false, reloadOnChange: true)
-                   .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                   .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-                   .AddEnvironmentVariables();
-            Configuration = builder.Build(); 
-        }
         public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        //public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        //{
+        //    var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+        //    builder.SetBasePath(environment.ContentRootPath)
+        //           .AddJsonFile("appsettings.json", false, reloadOnChange: true)
+        //           .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+        //           .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+        //           .AddEnvironmentVariables();
+        //    Configuration = builder.Build(); 
+        //}   
+       
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot(Configuration);//有参数的ocelot是需要引用Ocelot.Provider.Consul和Ocelot.Provider.Polly的，因为有参数的是和Consul查询服务结合的
+            services.AddOcelot(new ConfigurationBuilder()
+                 .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true).Build());//有参数的ocelot是需要引用Ocelot.Provider.Consul和Ocelot.Provider.Polly的，因为有参数的是和Consul查询服务结合的
             services.AddControllers();
         }
 
